da3e5abb18dff3cdc6889c1dc32fb6fc3add99c2
shiyasvp92 <shiyasvp92>
2023-09-27 10:59:07 +0000

    Pushed from Locofy Builder

---
+++ src/pages/Frame.tsx
@@ -0,0 +1,268 @@
+import { FunctionComponent } from "react";
+import styles from "./Frame.module.css";
+
+const Frame: FunctionComponent = () => {
+  return (
+    <div className={styles.frameParent}>
+      <div className={styles.frame}>
+        <div className={styles.frame1}>
+          <div className={styles.frame2}>
+            <div className={styles.frameChild} />
+          </div>
+          <div className={styles.frame3}>
+            <img
+              className={styles.roomindoPngLogo1}
+              alt=""
+              src="/roomindo-png-logo-1@2x.png"
+            />
+          </div>
+          <div className={styles.frame4}>
+            <div className={styles.frameItem} />
+          </div>
+          <div className={styles.frame5}>
+            <div className={styles.signUp}>Sign up</div>
+          </div>
+          <div className={styles.frame6}>
+            <div className={styles.findYourIdealContainer}>
+              <p className={styles.findYourIdeal}>
+                <span className={styles.colivingSpaceAnd}>
+                  <span>
+                    <b className={styles.findYourIdeal2}>Find Your Ideal</b>
+                  </span>
+                  <span className={styles.span}>
+                    <span>{` `}</span>
+                  </span>
+                </span>
+              </p>
+              <p className={styles.colivingSpaceAndManageItH}>
+                <span className={styles.colivingSpaceAnd}>
+                  <span className={styles.span}>
+                    <span>Coliving</span>
+                    <span className={styles.and}>{` `}</span>
+                    <span className={styles.space}>Space</span>
+                    <span className={styles.and}>{` and `}</span>
+                  </span>
+                </span>
+                <span className={styles.manageItHassleFreeWithUs}>
+                  <span className={styles.manage}>
+                    <b className={styles.findYourIdeal2}>Manage</b>
+                  </span>
+                  <span className={styles.itHassleFreeWithUs}>
+                    <span className={styles.manage}>{` `}</span>
+                    <span className={styles.itHassleFreeWith}>
+                      It Hassle-Free with Us
+                    </span>
+                  </span>
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className={styles.frame7}>
+            <div className={styles.howDoesIt}>How does it work</div>
+          </div>
+          <div className={styles.frame8}>
+            <div className={styles.aDetailedAnd}>
+              A Detailed and Easy-to-Follow Guide on How to Use Our Mobile
+              Application
+            </div>
+          </div>
+          <div className={styles.frame9}>
+            <img
+              className={styles.fmhInJustAClick21}
+              alt=""
+              src="/fmh-in-just-a-click--2-1@2x.png"
+            />
+          </div>
+          <img
+            className={styles.fmhInJustAClick31}
+            alt=""
+            src="/fmh-in-just-a-click--3-1@2x.png"
+          />
+          <img
+            className={styles.fmhInJustAClick31}
+            alt=""
+            src="/fmh-in-just-a-click--3-2@2x.png"
+          />
+          <div className={styles.frame10}>
+            <div className={styles.frameInner} />
+          </div>
+          <div className={styles.frame11}>
+            <div className={styles.frameInner} />
+          </div>
+          <div className={styles.frame12}>
+            <div className={styles.frameChild1} />
+          </div>
+          <div className={styles.frame13}>
+            <div className={styles.howDoesIt}>Smart search</div>
+          </div>
+          <div className={styles.frame14}>
+            <img
+              className={styles.fmhInJustAClick41}
+              alt=""
+              src="/fmh-in-just-a-click--4-1@2x.png"
+            />
+          </div>
+          <div className={styles.frame15}>
+            <div className={styles.enterYourDesired}>
+              Enter your desired area or home type in the search bar, and we'll
+              provide the best options
+            </div>
+          </div>
+          <div className={styles.frame16}>
+            <div className={styles.howDoesIt}>Legal Protection</div>
+          </div>
+          <div className={styles.frame17}>
+            <div className={styles.weTailorContracts}>
+              We tailor contracts for strong legal protection for both subletter
+              and sublessee
+            </div>
+          </div>
+          <div className={styles.frame18}>
+            <div className={styles.howDoesIt}>Easy as A,B,C</div>
+          </div>
+          <div className={styles.frame19}>
+            <div className={styles.selectYourDesired}>
+              Select your desired property, contact the owner, and make a deal!
+            </div>
+          </div>
+          <div className={styles.frame20}>
+            <img
+              className={styles.fmhInJustAClick101}
+              alt=""
+              src="/fmh-in-just-a-click--10-1@2x.png"
+            />
+          </div>
+          <div className={styles.frame21}>
+            <img
+              className={styles.fmhInJustAClick101}
+              alt=""
+              src="/fmh-in-just-a-click--2-2@2x.png"
+            />
+          </div>
+        </div>
+        <div className={styles.frame22}>
+          <div className={styles.frame23}>
+            <img className={styles.frameIcon} alt="" src="/frame.svg" />
+            <div className={styles.frame24}>
+              <img
+                className={styles.fmhInJustAClick61}
+                alt=""
+                src="/fmh-in-just-a-click--6-1@2x.png"
+              />
+            </div>
+          </div>
+          <div className={styles.frame25}>
+            <div className={styles.rentCollection}>Rent Collection</div>
+            <div className={styles.enablesLandlordsTo}>
+              Enables landlords to collect rent payments online, set up
+              automated reminders, and track payment history.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.frame26}>
+        <div className={styles.frame27}>
+          <div className={styles.frame28}>
+            <div className={styles.frame29}>
+              <div className={styles.rentCollection}>Maintenance Requests</div>
+              <div className={styles.tenantsCanSubmit}>
+                Tenants can submit maintenance requests through the app, and
+                landlords can track and manage these requests efficiently.
+              </div>
+            </div>
+            <div className={styles.frame30}>
+              <div className={styles.ellipseDiv} />
+              <img
+                className={styles.fmhInJustAClick51}
+                alt=""
+                src="/fmh-in-just-a-click--5-1@2x.png"
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.frame31}>
+          <div className={styles.frame32}>
+            <div className={styles.frameChild2} />
+            <div className={styles.communicationHub}>Communication Hub</div>
+            <div className={styles.facilitatesCommunicationBetwContainer}>
+              <p
+                className={styles.findYourIdeal}
+              >{`Facilitates communication between landlords, property managers, and `}</p>
+              <p className={styles.findYourIdeal}>
+                tenants through messaging and notifications.
+              </p>
+            </div>
+            <img
+              className={styles.fmhInJustAClick71}
+              alt=""
+              src="/fmh-in-just-a-click--7-1@2x.png"
+            />
+          </div>
+        </div>
+      </div>
+      <div className={styles.frame33}>
+        <div className={styles.frame34}>
+          <div className={styles.frame35}>
+            <div className={styles.frameChild3} />
+            <div className={styles.kycStorage}>KYC Storage</div>
+            <div className={styles.offersASecureContainer}>
+              <p
+                className={styles.findYourIdeal}
+              >{`Offers a secure platform for storing important documents like leases, `}</p>
+              <p className={styles.findYourIdeal}>
+                inspection reports, and maintenance records.
+              </p>
+            </div>
+            <img
+              className={styles.fmhInJustAClick81}
+              alt=""
+              src="/fmh-in-just-a-click--8-1@2x.png"
+            />
+          </div>
+        </div>
+        <div className={styles.frame36}>
+          <div className={styles.frameChild4} />
+          <img
+            className={styles.roomindoPngLogo2}
+            alt=""
+            src="/roomindo-png-logo-2@2x.png"
+          />
+          <div className={styles.contactUs}>Contact Us</div>
+          <div className={styles.roomindoIsA}>
+            ROOMINDO is a property management platform bringing all aspects of
+            managing shared living properties and housemates into one workflow.
+          </div>
+          <img className={styles.phoneIcon} alt="" src="/phone@2x.png" />
+          <img className={styles.mailIcon} alt="" src="/mail@2x.png" />
+          <div className={styles.contactfindmyhostelin}>
+            contact@findmyhostel.in
+          </div>
+          <div className={styles.div}>+91 88917 07303</div>
+          <img className={styles.facebookIcon} alt="" src="/facebook@2x.png" />
+          <img
+            className={styles.instagramIcon}
+            alt=""
+            src="/instagram@2x.png"
+          />
+          <img className={styles.linkedinIcon} alt="" src="/linkedin@2x.png" />
+          <img className={styles.youtubeIcon} alt="" src="/youtube@2x.png" />
+        </div>
+        <div className={styles.frame37}>
+          <div className={styles.maskGroup}>
+            <img className={styles.getItOn1} alt="" src="/get-it-on-1@2x.png" />
+            <div className={styles.downloadOurAppForContainer}>
+              <p className={styles.findYourIdeal}>
+                <b>DOWNLOAD</b>
+              </p>
+              <p className={styles.ourAppFor}>our app for best experience</p>
+            </div>
+          </div>
+          <img className={styles.pngwing1Icon} alt="" src="/pngwing-1@2x.png" />
+          <img className={styles.pngwing2Icon} alt="" src="/pngwing-2@2x.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Frame;
